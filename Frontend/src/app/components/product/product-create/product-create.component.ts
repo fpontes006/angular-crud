7088f6f81947d6aa64182ab536a5b5afd54486df
fpontes006 <fpontes006@gmail.com>
2022-08-16 16:50:35 -0300

    criando service e snackbar

--- Frontend/src/app/components/product/product-create/product-create.component.ts
+++ Frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service'
 
 @Component({
   selector: 'app-product-create',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+ 
 
-  constructor() { }
+  constructor(private productService: ProductService,
+              private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto Criado!')
+  }
+
+  cancel(): void {
+
+    this.router.navigate(['/products'])
+
   }
 
 }
